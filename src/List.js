83f483f5cdb2f90d43a6074c375c3d86a2593ddf
Rahel <raheline@hotmail.com>
2020-04-18 13:20:39 +0300

    Make list into table.

--- src/List.js
+++ src/List.js
@@ -46,47 +46,70 @@ function List({ list, setList, emptyRow }) {
         <>
             <button onClick={addRow}>+</button>{" "}
             <button onClick={clear}>Clear</button> <br />
-            {list.map((el, index) => (
-                <div className="row" key={index}>
-                    <input
-                        style={{ width: "9em" }}
-                        type="text"
-                        value={el.name}
-                        placeholder="Name"
-                        onChange={e =>
-                            onChangeData("name", e.target.value, index)
-                        }
-                    />
-                    <input
-                        style={{ width: "3em", textAlign: "center" }}
-                        value={el.date}
-                        placeholder="Date"
-                        onChange={e => onChangeDate(e.target.value, index)}
-                    />
-                    <input
-                        style={{ width: "3em", textAlign: "right" }}
-                        type="number"
-                        min="0"
-                        value={el.grams}
-                        placeholder="Grams"
-                        onChange={e =>
-                            onChangeData("grams", e.target.value, index)
-                        }
-                    />
-
-                    <input
-                        style={{ width: "2em", textAlign: "right" }}
-                        type="number"
-                        min="1"
-                        value={el.amount}
-                        placeholder="Amount"
-                        onChange={e =>
-                            onChangeData("amount", e.target.value, index)
-                        }
-                    />
-                    <button onClick={() => removeRow(index)}>-</button>
-                </div>
-            ))}
+            <table style={{margin: "0 auto"}}>
+                <tr>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Grams</th>
+                    <th>Amount</th>
+                </tr>
+                {list.map((el, index) => (
+                    <tr className="row" key={index}>
+                        <td>
+                            <input
+                                style={{ width: "9em" }}
+                                type="text"
+                                value={el.name}
+                                placeholder="Name"
+                                onChange={(e) =>
+                                    onChangeData("name", e.target.value, index)
+                                }
+                            />
+                        </td>
+                        <td>
+                            <input
+                                style={{ width: "3em", textAlign: "center" }}
+                                value={el.date}
+                                placeholder="Date"
+                                onChange={(e) =>
+                                    onChangeDate(e.target.value, index)
+                                }
+                            />
+                        </td>
+                        <td>
+                            <input
+                                style={{ width: "3em", textAlign: "right" }}
+                                type="number"
+                                min="0"
+                                value={el.grams}
+                                placeholder="Grams"
+                                onChange={(e) =>
+                                    onChangeData("grams", e.target.value, index)
+                                }
+                            />
+                        </td>
+                        <td>
+                            <input
+                                style={{ width: "2em", textAlign: "right" }}
+                                type="number"
+                                min="1"
+                                value={el.amount}
+                                placeholder="Amount"
+                                onChange={(e) =>
+                                    onChangeData(
+                                        "amount",
+                                        e.target.value,
+                                        index
+                                    )
+                                }
+                            />
+                        </td>
+                        <td>
+                            <button onClick={() => removeRow(index)}>-</button>
+                        </td>
+                    </tr>
+                ))}
+            </table>
         </>
     );
 }
