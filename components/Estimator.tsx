996ac8cc51ea390e207295599170c618ff96b19b
Rahel <raheline@hotmail.com>
2021-12-23 19:37:45 +0200

    remove error showing and add some TS types

--- components/Estimator.tsx
+++ components/Estimator.tsx
@@ -1,49 +1,40 @@
-import { useMemo, useState } from "react";
+import { ChangeEventHandler, useMemo, useState } from "react";
 import List from "./List";
 import Results from "./Results";
 import { useLocalStorage } from "../composables/useLocalStorage";
 
 function Estimator() {
-  let err;
   const { load, save } = useLocalStorage();
   const savedPerDay = useMemo(() => {
     const saved = load("perDay");
     return parseInt(saved, 10);
-  }, []);
+  }, [load]);
 
   const emptyRow = { name: "", date: "", grams: "", amount: 1 };
   const [list, setList] = useState(load("list") || [{ ...emptyRow }]);
-  const [perDay, setPerDay] = useState(savedPerDay || 1000);
+  const [perDay, setPerDay] = useState<number>(savedPerDay || 1000);
 
-  if (err) {
-    return <>Problem: {JSON.stringify(err)}</>;
-  }
+  const onChangePerDay: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const value = e.target.value;
+    setPerDay(parseInt(value, 10));
+    save("perDay", value);
+  };
 
-  try {
-    function onChangePerDay(e) {
-      const value = e.target.value;
-      setPerDay(value);
-      save("perDay", value);
-    }
-
-    return (
-      <div className="App">
-        <h2>Food estimator!</h2>
-        Food consumed per day:{" "}
-        <input
-          value={perDay}
-          onChange={onChangePerDay}
-          style={{ width: "60px", textAlign: "center" }}
-        />{" "}
-        grams
-        <List list={list} setList={setList} emptyRow={emptyRow} />
-        <Results list={list} setList={setList} perDay={perDay} />
-      </div>
-    );
-  } catch (error) {
-    err = error;
-    return <>Problem: {JSON.stringify(error)}</>;
-  }
+  return (
+    <div className="App">
+      <h2>Food estimator!</h2>
+      Food consumed per day:{" "}
+      <input
+        value={perDay}
+        type="number"
+        onChange={onChangePerDay}
+        style={{ width: "60px", textAlign: "center" }}
+      />{" "}
+      grams
+      <List list={list} setList={setList} emptyRow={emptyRow} />
+      <Results list={list} setList={setList} perDay={perDay} />
+    </div>
+  );
 }
 
 export default Estimator;
